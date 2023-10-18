a22271cc327a050823f61d5dfe3671f18e5dff47
Farzana <yesminfarzana.cse@gmail.com>
2023-10-18 21:35:33 +0600

    updated

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,9 +3,9 @@ import Left from './components/home/Left'
 
 const Home = () => {
   return (
-    <div className='w-[85%] h-[85%] bg-transparent text-white z-50 flex items-start justify-between'>
+    <div className='w-[95%] h-[95%] bg-transparent text-white z-50 flex items-start justify-between'>
         <div className='w-16 h-96 bg-bodyColor'></div>
-        <div className='w-[94%] h-full bg-transparent flex items-center'>
+        <div className='w-[95%] h-full bg-transparent flex items-center'>
           {/* left part start */}
           <Left/>
           {/* right part start */}
